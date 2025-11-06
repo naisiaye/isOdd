d3c5a42a8fdfe395fe8c996909a48a250825ce9b
Sharon <naisiayee@gmail.com>
2025-11-05 19:51:34 -0500

    created a function that checks for odd numbers and prnts out message

---
+++ index.js
@@ -0,0 +1,8 @@
+function isOdd(number) {
+  if (number % 2 === 1) {
+    return `That's an odd number`;
+  } else if (number % 2 === 0) {
+    return `Thats an even better number`;
+  }
+}
+console.log(isOdd(14));
