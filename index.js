91f8f1b001fb1360ec5ad9b627a1e18c62f94ed5
Sharon <naisiayee@gmail.com>
2025-11-05 20:46:32 -0500

    revised function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function isOdd(number) {
   if (number % 2 === 1) {
     return `That's an odd number`;
-  } else if (number % 2 === 0) {
+  } else {
     return `Thats an even better number`;
   }
 }
